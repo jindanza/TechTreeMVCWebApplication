86a058103ba0f759799ba20d669fe1cefd0867a0
Muhammad Jindan <jindanza7@gmail.com>
2024-10-08 08:07:41 +0700

    refactor(display content): update web design

---
+++ TechTreeMVCWebApplication/Controllers/ContentController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Linq;
+using TechTreeMVCWebApplication.Data;
+using TechTreeMVCWebApplication.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace TechTreeMVCWebApplication.Controllers
+{
+    public class ContentController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ContentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(int categoryItemId)
+        {
+            Content content = await (from item in _context.Content
+                                     where item.CategoryItem.Id == categoryItemId
+                                     select new Content
+                                     {
+                                         Title = item.Title,
+                                         VideoLink = item.VideoLink,
+                                         HTMLContent = item.HTMLContent,
+                                     }).FirstOrDefaultAsync();
+
+            return View(content);
+        }
+    }
+}
---
+++ TechTreeMVCWebApplication/Models/CategoryItemDetailsModel.cs
@@ -0,0 +1,17 @@
+﻿namespace TechTreeMVCWebApplication.Models
+{
+    public class CategoryItemDetailsModel
+    {
+        public int CategoryId { get; set; }
+        
+        public string CategoryTitle { get; set; }
+
+        public int CategoryItemId { get; set; }
+
+        public string CategoryItemTitle { get; set; }
+        
+        public string CategoryItemDescription { get; set; }
+
+        public string MediaImagePath { get; set; }
+    }
+}
